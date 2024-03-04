bf8d42bb598015f2003a4a855027062100fee6f8
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-05 03:01:19 +0600

    Add logout button and teacher mode button to navbar routes

--- app/(dashboard)/_components/navbarroutes.tsx
+++ app/(dashboard)/_components/navbarroutes.tsx
@@ -1,11 +1,34 @@
 'use client';
+import { Button } from '@/components/ui/button';
 import { UserButton } from '@clerk/nextjs';
+import { LogOut } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const NavbarRoutes = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isTeacherPage = pathname?.startsWith('/teacher');
+    const isPlayerPage = pathname?.includes('/chapter');
     return (
         <div className='flex gap-x-2 ml-auto'>
-            <UserButton />
+            {isTeacherPage || isPlayerPage ? (
+                <Button size="sm" variant="ghost">
+                    <LogOut className='h-4 w-4 mr-2' />
+                    Exit
+                </Button>
+            ) : (
+                <Link href='/teacher/courses'>
+                    <Button size="sm" variant="ghost">
+                        Teacher Mode
+                    </Button>
+                </Link>
+            )}
+            <UserButton
+                afterSignOut="/"
+            />
         </div>
     );
 };
